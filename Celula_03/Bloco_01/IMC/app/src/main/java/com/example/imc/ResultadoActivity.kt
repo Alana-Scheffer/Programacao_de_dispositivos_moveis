5ab3a4e84f99fffdffba20b5b1929b063ecbb253
Alana-Scheffer <scheffermell@gmail.com>
2026-06-08 23:13:07 -0300

    Add weight and height TextViews to ResultadoActivity
    Added UI components for displaying weight and height.

--- Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/ResultadoActivity.kt
+++ Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/ResultadoActivity.kt
@@ -9,22 +9,25 @@ class ResultadoActivity : AppCompatActivity() {
     private lateinit var titleDensidadeCorporal: TextView
     private lateinit var textViewHeaderDensidadeCorporal: TextView
     private lateinit var textViewResultadoDensidadeCorporal: TextView
+    private lateinit var textViewPeso: TextView
+    private lateinit var textViewAltura: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado2)
 
-        // Inicializando os componentes com findViewById
         titleDensidadeCorporal = findViewById(R.id.titleDensidadeCorporal)
         textViewHeaderDensidadeCorporal = findViewById(R.id.textViewHeaderDensidadeCorporal)
         textViewResultadoDensidadeCorporal = findViewById(R.id.textViewResultadoDensidadeCorporal)
+        textViewPeso = findViewById(R.id.textViewPeso)
+        textViewAltura = findViewById(R.id.textViewAltura)
 
-        // Obtendo o objeto IMC passado pela Intent
         val imc = intent.getParcelableExtra<IMC>("value")
 
-        // Configurando os textos dos componentes da UI
         titleDensidadeCorporal.text = imc?.nome.toString()
         textViewHeaderDensidadeCorporal.text = imc?.calcular()
-        textViewResultadoDensidadeCorporal.text = imc?.imc.toString()
+        textViewPeso.text = "Peso: ${imc?.peso}"
+        textViewAltura.text = "Altura: ${imc?.altura}"
+        textViewResultadoDensidadeCorporal.text = "IMC: %.2f".format(imc?.imc)
     }
 }
